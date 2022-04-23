29deea42185acbc9b9e1a20c716f8ef8c24f61fc
Danila <dkirillov2001@gmail.com>
2022-04-23 17:47:25 +0300

    Добавлена пагинация на закрепленные новости

--- src/Components/PinNews/PinNews.jsx
+++ src/Components/PinNews/PinNews.jsx
@@ -2,47 +2,52 @@ import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
 import New from "../New/New";
 import "./PinNew.css";
+import Pagination from "../Pagination/Pagination";
 
 const PinNew = () => {
   const [newPins, setNewPins] = useState([{ id: 0 }]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [newsPerPage] = useState(2);
 
   var pinnedNews = [];
   if (localStorage.getItem("bookmarks")) {
     pinnedNews = JSON.parse(localStorage.getItem("bookmarks"));
   }
 
-  // useEffect(() => {
-  //   let stringOfArr = "";
-
-  //   for (let i = 0; i < pinnedNews.length; i++) {
-  //     stringOfArr += `id_in=${pinnedNews[i]}&`;
-  //   }
-
-  //   axios
-  //     .get(`https://api.spaceflightnewsapi.net/v3/articles?` + stringOfArr)
-  //     .then((res) => {
-  //       setNewPins(res.data);
-  //     });
-  // }, []);
-
   useEffect(() => {
     setNewPins(pinnedNews);
   }, []);
 
-  debugger
+  const lastNewsIndex = currentPage * newsPerPage;
+  const firstNewIndex = lastNewsIndex - newsPerPage;
+  const currentNews = newPins.slice(firstNewIndex, lastNewsIndex);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   if (pinnedNews.length != 0) {
     return (
       <div>
-        {newPins.map((newPins) => (
-          <New news={newPins} key={newPins.id} />
-        ))}
+        <div>
+          {currentNews.map((newPins) => (
+            <New news={newPins} key={newPins.id} />
+          ))}
+        </div>
+        <Pagination 
+            newsPerPage = {newsPerPage}
+            totalNews = {newPins.length}
+            paginate = {paginate}
+            currentPage = {currentPage}
+        />
       </div>
     );
   } else {
     return (
       <>
-        <h1 className="emptyPinnedNews">Список ваших закрепленных новостей пуст!</h1>
-      </>);
+        <h1 className="emptyPinnedNews">
+          Список ваших закрепленных новостей пуст!
+        </h1>
+      </>
+    );
   }
 };
 
